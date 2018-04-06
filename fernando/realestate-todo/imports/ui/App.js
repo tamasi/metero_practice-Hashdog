ee281e6d85460d950c200f9f31496686b1f77bb0
Fernando Tripicchio <fernando.rt@gmail.com>
2018-04-06 19:16:41 -0300

    Add login, user and fields in estates

--- fernando/realestate-todo/imports/ui/App.js
+++ fernando/realestate-todo/imports/ui/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Properties } from '../api/properties.js';
 
 import Property from './Property.js';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 // App component - represents the whole app
 class App extends Component {
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    const address = ReactDOM.findDOMNode(this.refs.addressInput).value.trim();
+    console.log("llega");
+    Properties.insert({
+      text,
+      address,
+      createdAt: new Date(), // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username,
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    ReactDOM.findDOMNode(this.refs.addressInput).value = ''
+  }
+
   renderProperties() {
    return this.props.properties.map((property) => (
       <Property key={property._id} property={property} />
@@ -19,6 +42,30 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Lista de Propiedades</h1>
+
+          <AccountsUIWrapper />
+
+          { this.props.currentUser ?
+            <form className="new-property" onSubmit={this.handleSubmit.bind(this)} >
+             <label>
+              Description:
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Description"
+              />
+             </label>
+             <label>
+              Address:
+              <input
+                type="text"
+                ref="addressInput"
+                placeholder="Address"
+              />
+             </label>
+             <button type="submit" value="Submit">Guardar</button>
+            </form> : ''
+          }
         </header>
 
         <ul>
@@ -32,6 +79,7 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    properties: Properties.find({}).fetch(),
+    properties: Properties.find({}, { sort: { createdAt: -1 } }).fetch(),
+    currentUser: Meteor.user()
   };
 })(App);
--- fernando/realestate-todo/imports/ui/Property.js
+++ fernando/realestate-todo/imports/ui/Property.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 
 
+import { Properties } from '../api/properties.js';
+
 // Task component - represents a single todo item
 export default class Property extends Component {
+
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Properties.update(this.props.property._id, {
+      $set: { checked: !this.props.property.checked },
+    });
+  }
+
+  deleteThisProperty() {
+    Properties.remove(this.props.property._id);
+  }
+
   render() {
+    const propertyClassName = this.props.property.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.property.text}</li>
+      <li className={propertyClassName}>
+        <button className="delete" onClick={this.deleteThisProperty.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.property.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">
+          {this.props.property.text} <strong>Owner: {this.props.property.username}</strong>:
+        </span>
+      </li>
     );
   }
 }
